95c68fe9bb389cfacb3b0e817fe5e5c00b18ca50
John McChesney TenEyck Jr <john.m.teneyck@gmail.com>
2026-05-03 14:43:50 -0500

    Add OMT favicon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,16 @@ export const metadata: Metadata = {
     default: "OMT",
     template: "%s | OMT"
   },
-  description: "OMT"
+  description: "OMT",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16.png", sizes: "16x16", type: "image/png" }
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }]
+  }
 };
 
 export default function RootLayout({
